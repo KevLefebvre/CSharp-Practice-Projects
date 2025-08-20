886474d39f8465c04b82492150395913665dfb22
KevLefebvre <kevinlefebvrejobs@gmail.com>
2025-08-19 21:25:46 -0500

    Added First Guessing Game
    First Guessing Game

---
+++ GuessingGame/GuessingGame/Program.cs
@@ -0,0 +1,40 @@
+﻿namespace GuessingGame
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string secretWord = "giraffe";
+            string guess = "";
+            int guessCount = 0;
+            int guessLimit = 3;
+            bool outOfGuesses = false;
+
+            while (guess != secretWord && !outOfGuesses) 
+            {
+                if(guessCount < guessLimit)
+                {
+                    Console.Write((guessLimit-guessCount) + " Guess Left Remaining - Enter guess: ");
+                    guess = Console.ReadLine();
+                    guessCount++;
+                }
+                else 
+                {
+                    outOfGuesses = true;
+                    break;
+                }
+            }
+            if (outOfGuesses)
+            {
+                Console.Write("You lose!");
+            }
+            else
+            {
+                Console.Write("You win!");
+            }
+               
+
+            Console.ReadLine();
+        }
+    }
+}
