66d2af4e29fa04290b913d1b3efe3fa5776abec5
KevLefebvre <kevinlefebvrejobs@gmail.com>
2025-08-20 01:43:40 -0500

    Added a Rock Paper Scissors Game
    This is a Rock Paper Scissor Game.
    Practiced do/while loops, classes, methods, if statements, comparison, rng implementation, user inputs.

---
+++ RockPaperScissors/Program.cs
@@ -0,0 +1,74 @@
+﻿/*
+  This is a Rock Paper Scissor Game.
+  Practiced do/while loops, classes, methods, if statements, comparison, rng implementation, user input.
+ */
+using System.Runtime.Intrinsics.Arm;
+
+namespace RockPaperScissors
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string playerChoice;
+            string rollResult;
+            string playAgain;
+            rngNumbers roll = new rngNumbers();
+            bool continueGame = true;
+
+            do
+            {
+
+                Console.Write("Rock, Paper or Scissors?: ");
+                playerChoice = Console.ReadLine()?.Trim().ToLower();
+
+                if (playerChoice != "rock" && playerChoice != "paper" && playerChoice != "scissors")
+                {
+                    Console.WriteLine("Invalid choice, please try again.");
+                    continue;
+                }
+
+                rollResult = roll.getRngChoice();
+                if (rollResult == playerChoice)
+                {
+                    Console.WriteLine("The computer chose: " + rollResult);
+                    Console.WriteLine("It's a Draw!");
+                }
+                else if ((rollResult == "rock" && playerChoice == "paper") ||
+                         (rollResult == "paper" && playerChoice == "scissors") ||
+                         (rollResult == "scissors" && playerChoice == "rock"))
+
+                {
+                    Console.WriteLine("The computer chose: " + rollResult);
+                    Console.WriteLine("You win!");
+                }
+                else
+                {
+                    Console.WriteLine("The computer chose: " + rollResult);
+                    Console.WriteLine("You Lose!");
+                }
+
+                Console.WriteLine("Play again?: Y/N");
+                playAgain = Console.ReadLine()?.Trim().ToLower();
+                if (playAgain == "n")
+                {
+                    continueGame = false;
+                }
+
+            }
+            while (continueGame);
+
+
+
+
+
+
+
+
+
+
+
+
+        }
+    }
+}
---
+++ RockPaperScissors/rngNumbers.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RockPaperScissors
+{
+    internal class rngNumbers
+    {
+        private readonly Random _random = new Random();
+        
+
+        public int GetRngNum(int min = 1, int maxExclusive = 4)
+        {           
+            return _random.Next(min, maxExclusive);   
+        }
+       public string getRngChoice()
+        {
+            int roll = GetRngNum(1, 4);
+            string result = "";
+
+            if (roll == 1)
+            {
+                result = "rock";
+ 
+            }
+            else if (roll == 2)
+            {
+                result = "paper";
+                
+            }
+            else if (roll == 3)
+            {
+                result = "scissors";
+                
+            }
+            return result;
+        }
+    }
+    
+    
+}
